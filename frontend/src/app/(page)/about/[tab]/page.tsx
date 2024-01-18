c1c3c70af8ccc21994527514645c4ef8784e862a
Tilak Thapa <iamjrtilak@gmail.com>
2024-01-18 16:23:38 +0545

    Update AboutTab component with aboutMd prop

--- frontend/src/app/(page)/about/[tab]/page.tsx
+++ frontend/src/app/(page)/about/[tab]/page.tsx
@@ -27,7 +27,7 @@ const Page = async ({ params }: { params: { tab: string } }) => {
         {(() => {
           switch (activeTab) {
             case "about":
-              return <AboutTab />;
+              return <AboutTab aboutMd={ABOUT_JSON.aboutMd} />;
             case "skills":
               return <SkillsTab activeTab="techs" />;
             case "education":
@@ -37,7 +37,7 @@ const Page = async ({ params }: { params: { tab: string } }) => {
             case "experience":
               return <ExperienceTab />;
             default:
-              return <AboutTab />;
+              return <AboutTab aboutMd={ABOUT_JSON.aboutMd} />;
           }
         })()}
       </div>
--- frontend/src/app/(page)/about/page.tsx
+++ frontend/src/app/(page)/about/page.tsx
@@ -1,16 +1,15 @@
 import Profile from "./_components/ProfileComp/Profile";
 import TabsComp from "./_components/Tabs/TabButtons";
 import { Metadata } from "next";
-import { AboutDocType } from "@/types/others/about.types";
 import AboutTab from "./_components/AboutTab/AboutTab";
-import aboutJSON from "@/assets/json/about";
+import ABOUT_JSON from "@/assets/json/about";
 
 const page = async () => {
-  const about = await getAboutDoc();
+  const about = ABOUT_JSON;
   return (
     <>
       <div className="flex flex-col mx-auto gap-4 items-center w-full md:w-[80vw] min-h-screen">
-        <Profile about={about} />
+        <Profile />
         <hr className="w-full h-[0.5px] mt-32 sm:mt-20 bg-gray-200 border-0 rounded dark:bg-gray-700" />
         <TabsComp activeTab="about" />
         <AboutTab aboutMd={about?.aboutMd} />
@@ -22,7 +21,7 @@ export default page;
 
 //generate metadata for the page
 export const generateMetadata = async (): Promise<Metadata> => {
-  const about = await getAboutDoc();
+  const about = ABOUT_JSON;
   return {
     title: `About | ${about?.nickname}`,
     openGraph: {
@@ -34,19 +33,4 @@ export const generateMetadata = async (): Promise<Metadata> => {
       ],
     },
   };
-};
-
-const getAboutDoc = async () => {
-  const about = aboutJSON;
-  const mdRes = await fetch(about.aboutMd, {
-    method: "GET",
-    next: {
-      revalidate: 3600,
-    },
-  });
-  const mdContent = await mdRes.text();
-  return {
-    ...about,
-    aboutMd: mdContent,
-  } as AboutDocType;
 };
