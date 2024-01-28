b09ec0c689f86017f60d7a11062400cc0b4188e0
Tilak Thapa <iamjrtilak@gmail.com>
2024-01-28 21:43:28 +0545

    Add self-learning experience

--- src/assets/json/experience.ts
+++ src/assets/json/experience.ts
@@ -1,6 +1,13 @@
 const EXPERIENCE = [
   {
     status: "Ongoing",
+    date: "10/2022",
+    title: "Self Learning",
+    desc: "I started learning web development on my own. I have been learning and building projects since then. I have learnt MERN stack till now. I am continuously learning and improving my skills.",
+    _id: "Self_Learning_2022-10-01-00:00:00",
+  },
+  {
+    status: "Ongoing",
     date: "01/2024",
     title: "Web Dev Internship",
     desc: "I am currently working as a Web Dev Intern at a company called Prodigy InfoTech.",
