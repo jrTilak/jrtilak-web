df07d20c16b4573e4fbbfdc1ece9fd197df28912
jrtilak <iamjrtilak@gmail.com>
2024-05-31 09:18:19 +0545

    chore: update resume

--- src/assets/json/testimonials.ts
+++ src/assets/json/testimonials.ts
@@ -1,36 +1,35 @@
-// const TESTIMONIALS = [
-//   {
-//     img: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fHww",
-//     name: "John Doe",
-//     title: "Accountant",
-//     link: "",
-//     review:
-//       "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system and expound the actual teachings of the great explorer of the truth, the master- builder of human happiness.",
-//   },
-//   {
-//     img: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fHww",
-//     name: "John Smith",
-//     title: "Journalist, HWO News",
-//     link: "",
-//     review:
-//       "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system and expound the actual teachings.",
-//   },
-//   {
-//     img: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fHww",
-//     name: "Tamara Bellis",
-//     title: "Managing Director, JTH",
-//     link: "",
-//     review:
-//       "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system.",
-//   },
-// ];
-
-const TESTIMONIALS = [] as {
+const TESTIMONIALS: Testimonial[] = [
+  // {
+  //   img: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fHww",
+  //   name: "John Doe",
+  //   title: "Accountant",
+  //   link: "",
+  //   review:
+  //     "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system and expound the actual teachings of the great explorer of the truth, the master- builder of human happiness.",
+  // },
+  // {
+  //   img: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fHww",
+  //   name: "John Smith",
+  //   title: "Journalist, HWO News",
+  //   link: "",
+  //   review:
+  //     "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system and expound the actual teachings.",
+  // },
+  // {
+  //   img: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fHww",
+  //   name: "Tamara Bellis",
+  //   title: "Managing Director, JTH",
+  //   link: "",
+  //   review:
+  //     "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system.",
+  // },
+];
+type Testimonial = {
   img: string;
   name: string;
   title: string;
-  link: string;
+  link?: string;
   review: string;
-}[];
+};
 
 export default TESTIMONIALS;
